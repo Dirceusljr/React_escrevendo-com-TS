fb6fc0a186a5163e861e0a4c1cea23b9dddfd8d8
Dirceusljr <dirjunior18@gmail.com>
2024-03-28 19:29:35 -0300

    subindo projeto final + aulas 6 e 7

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,15 +1,32 @@
 import React from "react";
-import style from './Botao.module.scss'
+import style from "./Botao.module.scss";
 
-class Botao extends React.Component<{ children: React.ReactNode, type?: "button" | "submit" | "reset" | undefined}> {
-    render() {
-        const {type = "button"} = this.props;
-        return (
-            <button type={type} className={style.botao}>
-                { this.props.children }
-            </button>
-        )
-    }
+interface Props {
+  children?: React.ReactNode;
+  type?: "button" | "submit" | "reset" | undefined;
+  onClick?: () => void;
 }
+
+function Botao({ children, type, onClick }: Props) {
+  return (
+    <button 
+        type={type} 
+        className={style.botao} 
+        onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+// class Botao1 extends React.Component<{ children: React.ReactNode, type?: "button" | "submit" | "reset" | undefined, onClick?: () => void}> {
+//     render() {
+//         const {type = "button", onClick } = this.props;
+//         return (
+//             <button type={type} className={style.botao} onClick={onClick}>
+//                 { this.props.children }
+//             </button>
+//         )
+//     }
+// }
 
 export default Botao;
--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,37 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Botao from "../Botao";
 import style from "./Formulario.module.scss";
 import { ITarefa } from "../../types/tarefas";
 import { v4 as uuidv4 } from 'uuid';
 
-class Formulario extends React.Component<{
-  setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>;
-}> {
-  state = {
-    tarefa: "",
-    tempo: "00:00",
-  };
+interface Props {
+  setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}
 
-  adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+function Formulario({ setTarefas }: Props) {
+  const [tarefa, setTarefa] = useState("");
+  const [tempo, setTempo] = useState("00:00");
+
+  function adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
     evento.preventDefault();
-    this.props.setTarefas((tarefasAntigas) => [
+    setTarefas(tarefasAntigas => 
+      [
       ...tarefasAntigas,
-      { ...this.state,
+      {
+        tarefa,
+        tempo,
         selecionado: false,
         completado: false,
-        id: uuidv4() },
+        id: uuidv4()
+      }
     ]);
-    this.setState({
-      tarefa: "",
-      tempo: "00:00",
-    });
-  }
+    setTarefa("")
+    setTempo("00:00")
+    };
 
-  render() {
-    return (
-      <form
+
+  return (
+    <form
         className={style.novaTarefa}
-        onSubmit={this.adicionarTarefa.bind(this)}
+        onSubmit={e => adicionarTarefa(e)}
       >
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
@@ -39,10 +41,8 @@ class Formulario extends React.Component<{
             type="text"
             name="tarefa"
             id="tarefa"
-            value={this.state.tarefa}
-            onChange={(evento) =>
-              this.setState({ ...this.state, tarefa: evento.target.value })
-            }
+            value={tarefa}
+            onChange={(evento) => setTarefa(evento.target.value)}
             placeholder="O que você quer estudar?"
             required
           />
@@ -53,10 +53,8 @@ class Formulario extends React.Component<{
             type="time"
             step="1"
             name="tempo"
-            value={this.state.tempo}
-            onChange={(evento) =>
-              this.setState({ ...this.state, tempo: evento.target.value })
-            }
+            value={tempo}
+            onChange={(evento) => setTempo(evento.target.value)}
             id="tempo"
             min="00:00:00"
             max="01:30:00"
@@ -66,7 +64,71 @@ class Formulario extends React.Component<{
         <Botao type="submit">Adicionar</Botao>
       </form>
     );
-  }
 }
 
 export default Formulario;
+
+// class Formulario1 extends React.Component<{
+//   setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>;
+// }> {
+//   state = {
+//     tarefa: "",
+//     tempo: "00:00",
+//   };
+
+//   adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+//     evento.preventDefault();
+//     this.props.setTarefas((tarefasAntigas) => [
+//       ...tarefasAntigas,
+//       { ...this.state,
+//         selecionado: false,
+//         completado: false,
+//         id: uuidv4() },
+//     ]);
+//     this.setState({
+//       tarefa: "",
+//       tempo: "00:00",
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <form
+//         className={style.novaTarefa}
+//         onSubmit={this.adicionarTarefa.bind(this)}
+//       >
+//         <div className={style.inputContainer}>
+//           <label htmlFor="tarefa">Adicione um novo estudo</label>
+//           <input
+//             type="text"
+//             name="tarefa"
+//             id="tarefa"
+//             value={this.state.tarefa}
+//             onChange={(evento) =>
+//               this.setState({ ...this.state, tarefa: evento.target.value })
+//             }
+//             placeholder="O que você quer estudar?"
+//             required
+//           />
+//         </div>
+//         <div className={style.inputContainer}>
+//           <label htmlFor="tempo">Tempo</label>
+//           <input
+//             type="time"
+//             step="1"
+//             name="tempo"
+//             value={this.state.tempo}
+//             onChange={(evento) =>
+//               this.setState({ ...this.state, tempo: evento.target.value })
+//             }
+//             id="tempo"
+//             min="00:00:00"
+//             max="01:30:00"
+//             required
+//           />
+//         </div>
+//         <Botao type="submit">Adicionar</Botao>
+//       </form>
+//     );
+//   }
+// }
